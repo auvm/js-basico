58611e38ec145fa230ed4e4c9f6735b5e0e794aa
auvm <angelurielvelascom@gmail.com>
2022-07-07 21:18:41 -0500

    añado operadores, casting y mas!

---
+++ Truthy_Falsy.js
@@ -0,0 +1,32 @@
+/*Valore que de manera predefinida
+ dan veradero o falso*/
+
+// Falso
+Boolean(NaN);
+false
+Boolean();
+false
+Boolean(null);
+false
+Boolean(undefined);
+false
+Boolean(false);
+false
+Boolean("");
+false
+
+//Verdadero
+Boolean("x");
+true
+Boolean(1);
+true
+Boolean(2);
+true
+Boolean(3);
+true
+Boolean([]);
+true
+Boolean({});
+true
+Boolean(function(){});
+true
---
+++ operadores.js
@@ -0,0 +1,63 @@
+
+// Operadores binarios - aritmeticos
+2 + 10
+12
+50 - 10
+40
+10 * 20
+200
+20 / 2
+10
+
+//operador de asignación
+var a = 1;
+undefined
+
+//operador de negación
+!false
+true
+
+//operador de comparación - compara que el contenido sea igual 
+3 == "3";
+true
+3 == 3;
+
+//operadore de comparación - extrictamente igual
+true
+3 === "3";
+false
+3 === 3;
+true
+5 < 3
+false
+5 > 3
+true
+5 <= 6
+true
+5 >= 6
+false
+
+// operadores lógicos
+a = 1, b = 0;
+0
+a && b;
+0
+a || b;
+1
+!a;
+false
+!b;
+true
+
+//operadores unitarios incremento/decremento
+// postfijos y prefijos
+var edad = 17;
+undefined
+edad++;
+17
+edad;
+18
+++edad;
+19
+--edad;
+18
